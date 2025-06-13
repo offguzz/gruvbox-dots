c25c6f2426766b665dc70214634a152b5247183a
offguzz <110783474+offguzz@users.noreply.github.com>
2025-06-13 16:17:37 -0300

    ADD: add sway config

---
+++ .config/sway/scripts/output.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Obtém a lista de dispositivos de saída de áudio
+sinks=$(pactl list sinks | awk ' /^\s*Name:/ { name = substr($0, index($0, $2)) } /^\s*Description:/ { desc = substr($0, index($0, $2)); print desc " [" name "]"; } ')
+
+# Usa o fuzzel no modo dmenu para selecionar a saída de som
+selected_sink=$(echo "$sinks" | rofi  -dmenu -p "Sound Output:" | awk -F'[][]' '{ print $2 }')
+
+# Se um dispositivo foi selecionado, define como saída padrão
+if [ -n "$selected_sink" ]; then
+  pactl set-default-sink "$selected_sink"
+  # notify-send "Saída de áudio alterada para: $selected_sink"
+else
+  notify-send "Nenhuma saída de áudio selecionada"
+fi
