8e87ca1531a16d2f07aed094ba1977426dfa5d39
offguzz <offguzz>
2025-07-19 02:43:48 -0300

    commit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,85 +1,229 @@
 vim.cmd[[
-set nu rnu cursorline nowrap ignorecase smartcase tabstop=2 shiftwidth=2 expandtab wildoptions=pum showcmd noswapfile incsearch foldmethod=indent undofile undolevels=10000 clipboard+=unnamed,unnamedplus nohlsearch linebreak
+" My Simple config
+
+" Options 
+set nu
+set rnu
+set cursorline 
+set nowrap
+set ignorecase
+set smartcase
+set tabstop=2
+set shiftwidth=2
+set expandtab
+set wildoptions=pum
+set showcmd
+set showcmdloc=statusline
+set nocompatible
+set noswapfile
+set incsearch
+set foldmethod=indent
+set splitkeep=cursor
+
+" Let's save undo info!
+if has('nvim')
+    " Neovim: use stdpath('state')/undo
+    let s:undodir = stdpath('state') . '/undo'
+else
+    " Vim: use ~/.vim/undo
+    let s:undodir = $HOME . '/.vim/undo'
+endif
+
+" Create undo directory if it doesn't exist
+if !isdirectory(s:undodir)
+    call mkdir(s:undodir, "p", 0700)
+endif
+
+" Set undodir with double slashes for Neovim (to enable multiple files)
+if has('nvim')
+    execute 'set undodir=' . fnameescape(s:undodir) . '//'
+else
+    execute 'set undodir=' . fnameescape(s:undodir)
+endif
+
+set undofile
+set undolevels=10000
+set clipboard+=unnamed,unnamedplus
 
 let mapleader="\ "  
 
+" Netrw Config
 let g:netrw_banner = 0
 
-nmap <silent> <leader>w <C-w>
+" Better Marks
+nmap <leader>1 `1
+nmap <leader>2 `2
+nmap <leader>3 `3
+nmap <leader>4 `4
+nmap <leader>5 `5
+nmap <leader>6 `6
+nmap <leader>7 `7
+nmap <leader>8 `8
+nmap <leader>9 `9
+nmap <leader>0 `0
 
-nmap <silent> <leader>1 `1
-nmap <silent> <leader>2 `2
-nmap <silent> <leader>3 `3
-nmap <silent> <leader>4 `4
-nmap <silent> <leader>5 `5
-nmap <silent> <leader>6 `6
-nmap <silent> <leader>7 `7
-nmap <silent> <leader>8 `8
-nmap <silent> <leader>9 `9
-nmap <silent> <leader>0 `0
+" vimrc
+nmap <leader>rr :so ~/.vimrc<Cr>
+nmap <leader>fc :e ~/.vimrc<Cr>
 
-nmap <silent> [b :bp!<Cr>
-nmap <silent> ]b :bn!<Cr>
+" Window Control 
+nmap <C-h> <C-w>h
+nmap <C-j> <C-w>j
+nmap <C-k> <C-w>k
+nmap <C-l> <C-w>l
+nmap <leader>w <C-w>
 
-nmap <silent> <leader><Cr> :Ex<Cr>
+" Grep
+nmap <leader>sg :grep -r --exclude-dir=.git --exclude-dir=node_modules  .<Left><Left>
 
-imap <silent> <C-a> <Home>
-imap <silent> <C-f> <Right>
-imap <silent> <C-p> <Up>
-imap <silent> <C-n> <Down>
-imap <silent> <C-b> <Left>
-imap <silent> <C-e> <End>
-imap <silent> <C-d> <Del>
-imap <silent> <C-/> <C-o>u
-imap <silent> <C-x><C-s> <C-o>:w<Cr>
+"Buffers 
+nmap [b :bp!<Cr>
+nmap ]b :bn!<Cr>
+nmap <leader>sb :b! 
 
-cnoremap <silent> <C-h> <BS>
-cnoremap <silent> <C-j> <Down>
-cnoremap <silent> <C-k> <Up>
-cnoremap <silent> <C-b> <Left>
-cnoremap <silent> <C-f> <Right>
-cnoremap <silent> <C-a> <Home>
-cnoremap <silent> <C-e> <End>
-cnoremap <silent> <C-d> <Delete>
-cnoremap <silent> <C-o> <C-f>
+" Vim File Explorer
+nmap <leader><Cr> :Ex<Cr>
 
-nmap <silent> ]q :cnext<Cr>
-nmap <silent> ]Q :clast<Cr>
-nmap <silent> [q :cprev<Cr>
-nmap <silent> [Q :cfirst<Cr>
-nmap <silent> <leader>q :copen<Cr>
-nmap <silent> <leader>Q :cclose<Cr>
+" Text Managment
+vmap <S-k> :m '<-2<CR>gv=gv
+vmap <S-j> :m '>+1<CR>gv=gv
+
+" Word Substitution
+nmap <leader>s/ :%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>
+nmap <leader>s. :s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>
+
+" Text Movement 
+nmap <C-d> <C-d>zz
+nmap <C-u> <C-u>zz
+nmap j gj
+nmap k gk
+vmap j gj
+vmap k gk
+
+" Search Movement
+nmap n nzz 
+nmap N Nzz 
+
+" Insert Mode - Emacs Readline
+imap <C-a> <Home>
+imap <C-f> <Right>
+imap <C-p> <Up>
+imap <C-n> <Down>
+imap <C-b> <Left>
+imap <C-e> <End>
+imap <C-d> <Del>
+" M-f
+execute "set <M-char-102>=\ef"
+imap <M-char-102> <C-o>w
+" M-b
+execute "set <M-char-98>=\eb"
+imap <M-char-98> <C-o>b
+" M-d
+execute "set <M-char-100>=\ed"
+imap <M-char-100> <C-o>dw
+imap <C-/> <C-o>u
+imap <C-x><C-s> <C-o>:w<Cr>
+
+" Command Mode Emacs Readline
+cnoremap <C-h> <BS>
+cnoremap <C-j> <Down>
+cnoremap <C-k> <Up>
+cnoremap <C-b> <Left>
+cnoremap <C-f> <Right>
+cnoremap <C-a> <Home>
+cnoremap <C-e> <End>
+cnoremap <C-d> <Delete>
+cnoremap <C-o> <C-f>
+
+" Quick Fix
+nmap ]q :cnext<Cr>
+nmap ]Q :clast<Cr>
+nmap [q :cprev<Cr>
+nmap [Q :cfirst<Cr>
+nmap <leader>q :copen<Cr>
+nmap <leader>Q :cclose<Cr>
+
+" Install vim-plug if not found
+let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
+if empty(glob(data_dir . '/autoload/plug.vim'))
+  silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif 
+
+" Run PlugInstall if there are missing plugins
+autocmd VimEnter * if len(filter(values(g:plugs), '!isdirectory(v:val.dir)'))
+  \| PlugInstall --sync | source $MYVIMRC
+\| endif 
 
 call plug#begin()
-Plug 'morhetz/gruvbox'
+" List your plugins here
+Plug 'ghifarit53/tokyonight-vim'
+ Plug 'morhetz/gruvbox'
+Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
 Plug 'junegunn/fzf.vim'
 Plug 'tpope/vim-fugitive'
 call plug#end()
 
-let g:fzf_colors =
-\  { 'fg':      ['fg', 'Normal'],
-\  'bg':      ['bg', 'Normal'],
-\  'query':   ['fg', 'Normal'],
-\  'hl':      ['fg', 'Comment'],
-\  'fg+':     ['fg', 'CursorLine', 'CursorColumn', 'Normal'],
-\  'bg+':     ['bg', 'CursorLine', 'CursorColumn'],
-\  'hl+':     ['fg', 'Statement'],
-\  'info':    ['fg', 'PreProc'],
-\  'border':  ['fg', 'Comment'],
-\  'prompt':  ['fg', 'Conditional'],
-\  'pointer': ['fg', 'Exception'],
-\  'marker':  ['fg', 'Keyword'],
-\  'spinner': ['fg', 'Label'],
-\  'header':  ['fg', 'Comment'] } 
-
-nmap <silent> <leader><space> :Files<Cr>
-nmap <silent> <leader>/ :RG<Cr>
-nmap <silent> <leader>, :Buffers<Cr>
-nmap <silent> <leader>uC :Colors<Cr>
-
-nmap <silent> <leader>G :Git<Cr>:only<Cr>
-
+" Theme Settings
 set termguicolors
 set background=dark
+let g:tokyonight_style = 'night' " available: night, storm
+let g:tokyonight_enable_italic = 1
+"colorscheme tokyonight
+" colorscheme retrobox
 colorscheme gruvbox
+" syntax off
+
+" FZF Settings
+let g:fzf_vim = {} 
+let g:fzf_colors =
+  \ { 'fg':      ['fg', 'Normal'],
+  \ 'bg':      ['bg', 'Normal'],
+  \ 'query':   ['fg', 'Normal'],
+  \ 'hl':      ['fg', 'Comment'],
+  \ 'fg+':     ['fg', 'CursorLine', 'CursorColumn', 'Normal'],
+  \ 'bg+':     ['bg', 'CursorLine', 'CursorColumn'],
+  \ 'hl+':     ['fg', 'Statement'],
+  \ 'info':    ['fg', 'PreProc'],
+  \ 'border':  ['fg', 'Comment'],
+  \ 'prompt':  ['fg', 'Conditional'],
+  \ 'pointer': ['fg', 'Exception'],
+  \ 'marker':  ['fg', 'Keyword'],
+  \ 'spinner': ['fg', 'Label'],
+  \ 'header':  ['fg', 'Comment'] } 
+
+" let g:fzf_layout = { 'down': '30%' }
+" autocmd! FileType fzf
+" autocmd  FileType fzf set laststatus=0 noshowmode noruler
+"   \| autocmd BufLeave <buffer> set laststatus=2 showmode ruler
+
+" let g:fzf_layout = { 'down':  '40%'}
+" let g:fzf_layout = { 'window': 'enew' }
+let g:fzf_vim.buffers_jump = 1
+let g:fzf_vim.preview_window = ['right,50%,<50(up,60%)', 'ctrl-o']
+" let g:fzf_vim.preview_window = ['up,60%', 'ctrl-o']
+" let g:fzf_vim.preview_window = []
+" let g:fzf_vim.files_options =   '--style fu ll --no-border'
+" let g:fzf_vim.buffers_options = '--style default'
+" let g:fzf_vim.rg_options =      '--style default'
+" let g:fzf_vim.fi les_options =   ['--style', 'minimal', '--no-border']
+" let g:fzf_vim.buffers_options = ['--style', 'minimal', '--no-border']
+" let g:fzf_vim.rg_options =      ['--style', 'minimal', '--no-border']
+" let g:fzf_vim.colors_options =      ['--style', 'minimal', '--no-preview']
+let g:fzf_vim.command_prefix = 'Fzf'
+
+" FZF Keymaps
+nmap <silent> <leader><space> :FzfFiles<Cr>
+nmap <silent> <leader>sw :FzfRG <C-r><C-w><Cr>
+nmap <silent> <leader>/ :FzfRG<Cr>
+nmap <silent> <leader>, :FzfBuffers<Cr>
+nmap <silent> <leader>uC :FzfColors<Cr>
+
+" Git Keymaps
+nmap <silent> <leader>G :Git<Cr>:only<Cr>
+nmap <silent> <leader>gg :!lazygit<Cr>
+nmap <leader>gp :!git pull<Cr>
+nmap <leader>gP :!git push<Cr>
+nmap <silent> <leader>gl :Git log<Cr>
+
 ]]
